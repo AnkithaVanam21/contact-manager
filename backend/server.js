ff37ccb8aa5ead3d693989cb61b30fdbfc081fbf
Ankitha Vanam <your-vanamankitha@gmail.com>
2026-01-02 19:42:26 +0530

    Configure MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+mongoose.connect(process.env.MONGO_URI)
+
 const app = express();
 app.use(cors());
 app.use(express.json());
